c63cdff565d40744dc0b492e7aa07c0e6b6a11db
Danish Jamil <danish@cygnismedia.com>
2016-11-22 01:09:34 +0500

    Fixed issue

--- src/CYException.php
+++ src/CYException.php
@@ -2,7 +2,7 @@
 
 namespace CYCouchDB;
 
-class SagException extends \Exception {
+class CYException extends \Exception {
   public function __construct($msg = "", $code = 0) {
     parent::__construct("CY Error: $msg", $code);
   }
